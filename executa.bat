36be7fd7bd2d8c3c8275e75ddf2b4afa43a520ac
Matheus Marins Bernardello <20.00286-6@maua.br>
2024-04-12 10:39:25 -0300

    teste

--- executa.bat
+++ executa.bat
@@ -1,3 +1,3 @@
 ::No BAT, colocar o seguinte código:
 @echo off
-PowerShell.exe -ExecutionPolicy Bypass -File "C:\Users\40416224\Desktop\Automation-Python\AtualizaRepo.ps1"
+PowerShell.exe -ExecutionPolicy Bypass -File "C:\Users\40416224\Desktop\AtualizaRepo.ps1"
