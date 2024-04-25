620bad8d5836ce789738fb05c63b46b27931f822
Matheus Marins Bernardello <matheus.bernardello@telefonica.com>
2024-04-25 12:12:47 -0300

    Atualizacao automatica feito pelo usuario redecorp\40416224: 25/04/2024 12:12

--- README.md
+++
@@ -1,2 +0,0 @@
-# Automation-Python
-A ideia é tentar otimizar atividades repetitivas com python
--- executa.bat
+++
@@ -1,3 +0,0 @@
-::No BAT, colocar o seguinte código:
-@echo off
-PowerShell.exe -ExecutionPolicy Bypass -File "C:\Users\40416224\Desktop\AtualizaRepo.ps1"
